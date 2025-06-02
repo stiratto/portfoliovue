532fbb7f2309716648de51db500d41c3e67765ed
stiratto <moranmoralesj4@gmail.com>
2025-06-02 10:15:29 -0500

    skills separated into a json, v-for on skills.vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,69 +1,34 @@
 <script setup>
 import { Package, Layout, SquareTerminal, Bot } from 'lucide-vue-next';
+import skillslist from '../consts/skills-list.ts'
+
 
 </script>
 
 
 <template>
-  <section class="about-section py-28 md:px-[9em] flex flex-col items-center justify-center ">
+  <section class="about-section py-28 md:px-[9em] flex flex-col
+    items-center justify-center border border-[#ffdd33]">
     <div>
-      <h1 class="text-4xl sm:text-7xl font-black text-green-300">
-        <span class="text-[#1c1c1c]">/</span>skills
+      <h1 class="text-4xl sm:text-7xl font-black text-[#ffdd33]">
+        <span class="text-[#95a99f]">/</span>skills
       </h1>
     </div>
     <div class="w-full flex flex-col gap-8 text-sm sm:text-lg max-w-2xl self-start">
-      <div class="w-full text-white gap-8 rounded-2xl p-4 flex flex-col justify-center">
-        <h1 class="flex items-center gap-2 text-5xl text-green-300 font-bold text-start tracking-tight">
-          <Package size=40 /> Backend
+      <div v-for="skill in skillslist" class="w-full text-white gap-8 rounded-2xl p-4 flex flex-col justify-center">
+        <h1 class="flex items-center gap-2 text-5xl text-[#95a99f] font-bold text-start tracking-tight">
+          <component :is="skill.skilltitleicon" class="w-12 h-12" />
 
+          {{ skill.skilltitle }}
         </h1>
-        <p class="text-start">
-          With experience on
-          Java
-          Springboot
-          NestJS,
-          ExpressJS
-          PostgreSQL
-          Prisma ORM, I've been able to build complete Web Apps.
+        <p class="text-start text-white">
+          {{ skill.skilldescription }}
         </p>
       </div>
-      <div class="w-full text-white gap-8 rounded-2xl p-4 flex flex-col justify-center">
-        <h1 class="text-5xl text-green-300 font-bold text-start tracking-tight flex  gap-2">
-          <Layout size=40 />
-          Frontend
-        </h1>
-        <p class="text-start">
-          All the Web Apps built by me are designed, maintained and developed by me.
-          Using tools like React on its majority, VueJS as a second tool and Typescript under the
-          hood because of its readability and static-typed-like
-          developing.
-        </p>
-      </div>
-      <div class="w-full text-white gap-8 rounded-2xl p-4 flex flex-col justify-center">
-        <h1 class="text-5xl text-green-300 font-bold text-start tracking-tight flex gap-2">
-          <SquareTerminal size=40 />
-          Linux
-        </h1>
-        <p class="text-start">
-          As a person that likes "freedom" when coming to personal environments and work
-          environments, I've discovered Linux like 3-2 years ago, it
-          has beem my main OS for 1-2 years. I've explored various distros
-          like GNOME, KDE and Arch which was my main
-          distro. Right now I use EndeavourOS with the i3 window
-          manager.
-        </p>
-      </div>
-      <div class="w-full text-white gap-8 rounded-2xl p-4 flex flex-col justify-center">
-        <h1 class="text-5xl text-green-300 font-bold text-start tracking-tight flex gap-2">
-          <Bot size=40 />
-          Automation
-        </h1>
-        <p class="text-start">
-          I've automated Spotify, Youtube, Uber Eats Manager.
-          I also automate daily tasks on my workflow, and it's easier
-          to do that on Linux.
-        </p>
-      </div>
+
+
+
+
     </div>
   </section>
 
---
+++ src/consts/skills-list.ts
@@ -0,0 +1,27 @@
+import { Package, Layout, SquareTerminal, Bot } from 'lucide-vue-next';
+
+const skillslist = [
+   {
+      skilltitle: "Backend",
+      skilltitleicon: Package,
+      skilldescription: 'With experience on Java Springboot NestJS, ExpressJS PostgreSQL, Prisma ORM, I\'ve been able to build complete Web Apps'
+   },
+   {
+      skilltitle: "Frontend",
+      skilltitleicon: Layout,
+      skilldescription: ' All the Web Apps built by me are designed, maintained and developed by me. \
+          Using tools like React on its majority, VueJS as a second tool and Typescript under the hood because of its readability and static-typed-like developing.'
+   },
+   {
+      skilltitle: "Linux",
+      skilltitleicon: SquareTerminal,
+      skilldescription: "As a person that likes \"freedom\" when coming to personal environments and work environments, I've discovered Linux like 3-2 years ago, it has beem my main OS for 1-2 years. I've explored various distros like GNOME, KDE and Arch which was my main distro. Right now I use EndeavourOS with the i3 window manager. "
+   },
+   {
+      skilltitle: "Automation",
+      skilltitleicon: Bot,
+      skilldescription: "I've automated Spotify, Youtube, Uber Eats Manager. I also automate daily tasks on my workflow, and it's easier to do that on Linux. "
+   }
+]
+
+export default skillslist;
